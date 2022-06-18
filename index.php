535f9f6a971b3e58855292f42e2c6a0efce0cc97
Hackerr7767 <97411928+Hackerr7767@users.noreply.github.com>
2022-06-18 17:04:00 +0200

    The minwind website

---
+++ index.php
@@ -0,0 +1,17 @@
+<?php
+use MinwindWebsite\PHPSource\HTMLElements\HTMLElements;
+
+$x = new HTMLElements();
+$x->h1("Welcome.");
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Minwind</title>
+<body>
+    <p>The Mozilla Public License, v3.1.</p>
+</body>
+</html>
